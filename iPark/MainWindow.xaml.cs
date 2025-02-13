61bb282b682bec149e331091deb8689557a4839c
root <root@Alaric-PC.>
2025-02-13 15:17:49 -0800

    initial commit

---
+++ iPark/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace iPark
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartClock();
+        }
+
+        private void StartClock()
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += (sender, e) =>
+            {
+                TimeText.Text = DateTime.Now.ToString("hh:mm:ss tt");
+                DateText.Text = DateTime.Now.ToString("MM/dd/yyyy");
+                WeekdayText.Text = DateTime.Now.DayOfWeek.ToString();
+            };
+            timer.Start();
+        }
+    }
+}
